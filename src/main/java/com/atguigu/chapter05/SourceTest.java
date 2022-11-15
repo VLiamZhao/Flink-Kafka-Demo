70bbc46b801fe40901ad8595719a0c38cff3f29b
Can Zhao <canzhao@CandeMBP.lan>
2022-11-15 00:33:06 -0600

    kafka properties

--- src/main/java/com/atguigu/chapter05/SourceTest.java
+++ src/main/java/com/atguigu/chapter05/SourceTest.java
@@ -1,9 +1,12 @@
 package com.atguigu.chapter05;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class SourceTest {
     public static void main(String[] args) throws Exception {
@@ -33,6 +36,13 @@ public class SourceTest {
 
         // real company big data source: Kafka
         // need to use flink-connector-kafka dependency
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+        DataStreamSource kafkaStream = env.addSource(new FlinkKafkaConsumer<String>("clicks", new SimpleStringSchema(), properties));
 
         stream2.print();
 
